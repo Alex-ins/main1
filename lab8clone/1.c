faee3a392b7a27268214cc9e0b99fa6bfc43f79a
Alexey <lesha.vitkovskiy.01@mail.ru>
2023-05-26 04:33:44 -0400

    Dobavlena vozmozhost vichitaniya chisel

---
+++ lab8clone/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+ int a = 5;
+ int b = 3;
+ int sum = a + b;
+ int difference = a - b;
+ printf("Summa: %d\n, sum);
+ printf("Raznost: %d\n, difference);
+ return 0;
+}
